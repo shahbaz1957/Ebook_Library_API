6e11c0ab9b9401c81694af9afe266d52181d4892
shahbaz1957 <shahbazjamia1957@gmail.com>
2025-10-21 18:40:25 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -5,3 +5,5 @@ const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
   console.log(`Server running on port..${PORT}`);
 });
+
+
--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,11 @@ const app = express();
 app.use(express.json());
 
 // Example route
-app.get('/', (req, res) => {
-  res.send('Ebook Library API is running!');
+app.get('/', (req, res,next) => {
+  res.json({message: 'Ebook Library API is running!'});
 });
+app.post('/',(req,res,next)=>{
+  res.send('Data Save Successfully !!!!!')
+})
 
 export default app;
