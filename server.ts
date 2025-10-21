eb8dc26badf7f22c510752981764e34a623e0f75
shahbaz1957 <shahbazjamia1957@gmail.com>
2025-10-21 18:03:40 +0530

    Now fully setup of .ts and .js conflect

--- server.ts
+++ server.ts
@@ -1,8 +1,7 @@
-// server.ts
-import app from "./src/app.ts";
+import app from "./src/app.js";
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}...`);
+  console.log(`Server running on port..${PORT}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
-// src/app.ts
 import express from 'express';
+
 const app = express();
 
+// Middleware
 app.use(express.json());
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('Ebook Library API is running!');
+});
 
 export default app;
