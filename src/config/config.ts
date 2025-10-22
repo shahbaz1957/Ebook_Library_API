e866cae80ed3add0385a6b2e94429166b34b51b7
shahbaz1957 <shahbazjamia1957@gmail.com>
2025-10-22 15:28:13 +0530

    sign with jwt token

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,8 @@ conf();
 const _config = {
   port: process.env.PORT,
   DB_URL: process.env.MONGO_URI_STRING,
-  ENV:process.env.NODE_ENV
+  ENV:process.env.NODE_ENV,
+  Jwt_Secret:process.env.JWT_SECRET
 };
 
 export const config = Object.freeze(_config);
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,9 +1,12 @@
 import type { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel.js";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const createUser = async(req: Request, res: Response, next: NextFunction) => {
+import { config } from "../config/config.js";
+
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
 
   // validation
@@ -13,19 +16,28 @@ const createUser = async(req: Request, res: Response, next: NextFunction) => {
   }
 
   // Check if User Already Exist
-  const user = await userModel.findOne({email})
+  const user = await userModel.findOne({ email });
 
-  if(user){
-    const error = createHttpError(400, "User Alredy Exist with this Email")
+  if (user) {
+    const error = createHttpError(400, "User Alredy Exist with this Email");
     return next(error);
   }
 
   // Hashed Password
-  const passwordHashed = bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  // Create Token
+  const token =  jwt.sign({ sub: newUser._id }, config.Jwt_Secret as string, {expiresIn: "7d"});
 
   res.status(200).json({
     message: "User Created Successfully !!!!",
+    accessToken: token
   });
 };
 
