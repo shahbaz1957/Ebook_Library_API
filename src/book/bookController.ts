748bc5bc3f434d88534216f84ac976b792de21c1
shahbaz1957 <shahbazjamia1957@gmail.com>
2025-10-23 16:58:09 +0530

    add cloudinary functionality

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,11 +1,36 @@
+import type { Response, Request, NextFunction } from "express";
+import cloudinary from "../config/cloudinary.js";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 
-import type { Response,Request,NextFunction } from "express"
-
-const createBook = async(req:Request,res:Response, next:NextFunction)=>{
-
-    const {} = req.body;
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  const files = req.files as { [filename: string]: Express.Multer.File[] };
     console.log(req.files)
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
 
-}
+  const cover = files.coverImage[0];
+  const coverPath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    cover.filename
+  );
 
-export {createBook};
+  const coverImageExt = files.coverImage[0].mimetype.split("/").at(-1);
+  const coverPublicId = files.coverImage[0].filename;
+
+  const uploadFilesOnCLoudinary = await cloudinary.uploader.upload(coverPath, {
+    folder: "book-cover",
+    public_id: coverPublicId, // filepath
+    resource_type: "image",
+    overwrite: true,
+    use_filename: false,
+    unique_filename: false,
+    format: coverImageExt,
+  });
+  return res.status(201).json({coverPublicId:coverPublicId, message: "Uploaded", data: uploadFilesOnCLoudinary });
+//   console.log("Upload on Cloudonary -> ", uploadFilesOnCLoudinary);
+//   console.log("coverPublicId .. -> ", coverPublicId);
+};
+
+export { createBook };
