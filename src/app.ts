d637a9dc09ee7ed8b56293ee681d1cd4f7cecc17
shahbaz1957 <shahbazjamia1957@gmail.com>
2025-10-25 23:41:33 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import createHttpError from "http-errors";
 import express from "express";
+import cors from "cors";
 import globalError from "./middlewares/globalErrorHandler.js";
 import userRouter from "./user/userRoute.js";
 import bookRouter from "./book/bookRoute.js";
@@ -10,17 +11,23 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        optionsSuccessStatus: 200,
+    })
+);
 
 // Here we Register All routes
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  const error = createHttpError(400, () => {
-    return next(error);
-  });
-  res.json({ message: "Ebook Library API is running!" });
+    const error = createHttpError(400, () => {
+        return next(error);
+    });
+    res.json({ message: "Ebook Library API is running!" });
 });
 
 app.post("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Data Save Successfully !!!!! ");
+    res.send("Data Save Successfully !!!!! ");
 });
 app.use("/api/users", userRouter);
 app.use("/api/books", bookRouter);
